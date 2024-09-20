bd147ca12987e621a651c21f13d39f7a8b68756d
Francesco Fevoli <francesdbz18pc@outlook.es>
2024-09-20 10:27:22 +0200

    AAAAAAAAAAAAAA 2

--- src/ejercicios1/RWtxt.java
+++ src/ejercicios1/RWtxt.java
@@ -4,27 +4,24 @@ import java.io.*;
 
 public class RWtxt {
 	public static void main(String[] args) throws IOException {
-		File fichero = new File("C:\\\\Users\\\\aludam2\\\\Documents\\\\sisirve.txt");
+		File fichero = new File("C:\\Users\\aludam2\\Documents\\sisirve.txt");
 		FileWriter fic = new FileWriter (fichero, true);
-		String nueva = " Alcorcón";
-		fic.write(nueva);
+		String nueva = "Salamanca" + "\n";
+		fic.append(nueva);
 		fic.close();
-		Scanner sc = new Scanner(new FileReader("C:\\Users\\aludam2\\Documents\\sisirve.txt")).useDelimiter(" ");
+		FileReader fr = new FileReader(fichero);
+		BufferedReader br = new BufferedReader(fr);
+        ArrayList<String> arrayList = new ArrayList<>();
         String str;
-        int i = 0;
-        String[] array = new String[13];
-        while (sc.hasNext()) {
-            str = sc.next();
-            array[i] = str;
-            i++;
-        }
-        sc.close();
-        Arrays.sort(array, Collections.reverseOrder());
+		while((str = br.readLine()) != null){
+    		arrayList.add(str);		
+    	}
+        br.close();
+        arrayList.sort(Collections.reverseOrder());
 		fic = new FileWriter (fichero);
-		for(int j=0; j<array.length; j++)
-		fic.write(array[j] +  " ");
+		for(String str2 : arrayList)
+		fic.write(str2 +  "\n");
 		fic.close();
 	}
 }
-
 /* Realizar un programa java que abra para añadir el fichero de ciudades creado anteriormente, y añada al final una nueva ciudad. A continuación, realizar un proceso que lea el fichero resultante, lo guarde en un array, lo vuelva a ordenar descendentemente y lo escriba de nuevo en el fichero, reemplazando el anterior. */
