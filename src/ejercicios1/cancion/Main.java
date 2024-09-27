81085c7a474d88211b9b0a0e200a743a1f4d48d8
Francesco Fevoli <francesdbz18pc@outlook.es>
2024-09-27 10:21:19 +0200

    mayonesa

--- src/ejercicios1/cancion/Main.java
+++ src/ejercicios1/cancion/Main.java
@@ -1,24 +1,29 @@
 package ejercicios1.cancion;
 
-import ejercicios1.Persona;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Cancion c;
         File fichero = new File("FichCancion.dat");
         FileOutputStream fileout = new FileOutputStream(fichero);
         ObjectOutputStream dataos = new ObjectOutputStream(fileout);
-        menu();
+        menu(dataos);
+        FileInputStream filein = new FileInputStream(fichero);
+        ObjectInputStream dataIS = new ObjectInputStream(filein);
+        Cancion c;
+        try {
+            while (true) {
+                c = (Cancion) dataIS.readObject();
+                c.mostrarDatosCancion();
+            }
+        } catch (EOFException | ClassNotFoundException eo) {
+            System.out.println("FIN DE LECTURA.");
+        }
+        dataIS.close();
     }
 
-    private static void menu() {
+    private static void menu(ObjectOutputStream dataos) throws IOException {
         int escribir = 1;
         Scanner sc = new Scanner(System.in);
         while (escribir != 0) {
@@ -26,9 +31,11 @@ public class Main {
             escribir = sc.nextInt();
             sc.nextLine();
             if (escribir == 1) {
-                //c = new Cancion(sc.nextInt(), sc.nextInt(), sc.nextLine(), sc.nextLine(), sc.nextLine(), (sc.nextInt()==1)?true:false);
+                dataos.writeObject(construirCancion());
             }
         }
+        dataos.close();
+
     }
 
     public static Cancion construirCancion() {
@@ -36,6 +43,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduzca el identificador de la canción: ");
         c.setId(sc.nextInt());
+        sc.nextLine();
+        System.out.print("Introduzca el año de publicación: ");
+        c.setAnyo(sc.nextInt());
+        sc.nextLine();
+        System.out.print("Introduzca el título de la canción: ");
+        c.setTitulo(sc.nextLine());
+        System.out.print("\nIntroduzca el artista: ");
+        c.setArtista(sc.nextLine());
+        System.out.print("\nIntroduzca la duración: ");
+        c.setDuracion(sc.nextLine());
+        System.out.print("¿La canción es española? Pulsa 1 para sí, 0 para no: ");
+        c.setEspanyol(sc.nextInt() == 1);
         return c;
     }
 }
